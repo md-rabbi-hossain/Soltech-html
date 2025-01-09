3ee70ccc72a4f291222d4e52c2dba5c224f4fde1
Md Rabbi Hossain <mdrabbihossain1443@gmail.com>
2025-01-10 03:14:41 +0600

    done

---
+++ .history/Js/Script_20250109011126.js
@@ -0,0 +1,8 @@
+window.onscroll = function () {
+  let navbar = document.querySelector("nav");
+  if (window.pageYOffset > 0) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+};
